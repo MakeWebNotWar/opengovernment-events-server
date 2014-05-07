78ebf367e9028f8477879837daa5a50e1d7e1f4a
Raymond Kao <ray@peopleandcode.com>
2014-05-07 17:19:46 -0400

    updated helper

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ApplicationController < ApplicationController
   include ApplicationHelper
-  before_filter :authenticate_user_from_token!
+  # before_filter :authenticate_user_from_token!
 
 end
