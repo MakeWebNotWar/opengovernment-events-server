0cc6515e2357ac93f625a8447dee442b5180e3e3
Raymond Kao <ray@peopleandcode.com>
2014-05-07 17:38:40 -0400

    update comment and event models to make it simpler for now

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -22,7 +22,7 @@ class Api::V1::CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:text, :event, :owner)
+    params.require(:comment).permit(:text, :event_id, :owner)
   end
 
 end
